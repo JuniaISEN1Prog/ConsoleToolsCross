cd288e7c709323eb9d5a863d550781dea0f65a05
Pascal RICQ <55278618+RicqPascal@users.noreply.github.com>
2026-09-23 15:21:56 +0200

    upd demo.h

--- ConsoleTools/demo.h
+++ ConsoleTools/demo.h
@@ -1,7 +1,7 @@
 
 #define Pi 3.14159265358979
 int clipping;
-int elipse(COORD centre, double rayonX, double rayonY);
-int circle(COORD centre, double rayon);
+//int elipse(COORD centre, double rayonX, double rayonY);
+//int circle(COORD centre, double rayon);
 
-int lissajous(COORD centre, double rayonX, double rayonY, double p, double q);
+//int lissajous(COORD centre, double rayonX, double rayonY, double p, double q);
